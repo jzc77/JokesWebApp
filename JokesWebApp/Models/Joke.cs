c0b13bbc2f1a0d4ea1508401069861ece5548821
JazC <jchan7713@gmail.com>
2023-02-28 20:46:52 -0800

    Add Joke class, add Joke database

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public int JokeQuestion { get; set; }
+        public int JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
